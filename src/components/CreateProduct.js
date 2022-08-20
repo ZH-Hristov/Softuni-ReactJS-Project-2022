66594161b066955f7c86ab11b7d9916f0cd1d7d4
GIGABABAIT <zhhristov@abv.bg>
2022-08-20 18:41:52 +0300

    Create page styling

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -19,50 +19,56 @@ const CreateProduct = () => {
     }
 
     return (
-        <div className={styles.createcontainer}>
-            <form id="create" onSubmit={onSubmit}>
-                <h1>Create Product Listing</h1>
-                <label htmlFor="product-title">Product name:</label>
-                <input
-                    type="text"
-                    id="title"
-                    name="title"
-                    placeholder="Enter Product Title"
-                />
-                <label htmlFor="product-size">Product Size:</label>
-                <select id="product-size" name="product-size">
-                    <option value="XS">XS</option>
-                    <option value="S">S</option>
-                    <option value="M">M</option>
-                    <option value="L">L</option>
-                    <option value="XL">XL</option>
-                </select>
-                <label htmlFor="product-price">Product Price:</label>
-                <input
-                    type="number"
-                    id="product-price"
-                    name="product-price"
-                    min={1}
-                    step={0.01}
-                    placeholder={4.99}
-                />
-                <label htmlFor="product-image-url">Product Image Link:</label>
-                <input
-                    type="text"
-                    id="product-image-url"
-                    name="image-url"
-                    placeholder="https://imgur.com/myImage.jpg"
-                />
-                <label htmlFor="product-additional-details">Additional Details:</label>
-                <textarea name="product-additional-details" id="product-additional-details" defaultValue="" />
+        <>
+            <h1>Create Product Listing</h1>
 
-                <input
-                    className="btn submit"
-                    type="submit"
-                    value="Create"
-                />
-            </form>
-        </div>
+            <div className={styles.createcontainer}>
+                <form id="create" className={styles.createform} onSubmit={onSubmit}>
+                    <label className={styles.createlabel} htmlFor="product-title">Product name:</label>
+                    <input
+                        className={styles.createinput}
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Enter Product Title"
+                    />
+                    <label className={styles.createlabel} htmlFor="product-size">Product Size:</label>
+                    <select id="product-size" name="product-size" className={styles.createinput}>
+                        <option value="XS">XS</option>
+                        <option value="S">S</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+                    </select>
+                    <label className={styles.createlabel} htmlFor="product-price">Product Price:</label>
+                    <input
+                        className={styles.createinput}
+                        type="number"
+                        id="product-price"
+                        name="product-price"
+                        min={1}
+                        step={0.01}
+                        placeholder={4.99}
+                    />
+                    <label className={styles.createlabel} htmlFor="product-image-url">Product Image Link:</label>
+                    <input
+                        className={styles.createinput}
+                        type="text"
+                        id="product-image-url"
+                        name="image-url"
+                        placeholder="https://imgur.com/myImage.jpg"
+                    />
+                    <label className={styles.createlabel} htmlFor="product-additional-details">Additional Details:</label>
+                    <textarea className={styles.createinput} name="product-additional-details" id="product-additional-details" defaultValue="" />
+
+                    <input
+                        className="btn submit"
+                        type="submit"
+                        value="Create"
+                    />
+                </form>
+            </div>
+        </>
     )
 }
 
