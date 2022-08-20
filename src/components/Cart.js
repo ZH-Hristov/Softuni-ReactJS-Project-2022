c966d63d30829c432d338800a013680b56d0ba2a
GIGABABAIT <zhhristov@abv.bg>
2022-08-20 09:24:08 +0300

    Cart styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -10,21 +10,28 @@ const Cart = () => {
 
     return (
         <>
-        <h1>Cart</h1>
+            <h1>My Cart</h1>
 
-        <div>
-            {Object.keys(cartItems).length > 0 ?
-            <ul className={styles.cartul}>
-                {Object.values(cartItems).map(x => 
-                <li className={styles.cartli} key={x._id} >
-                    <img className={styles.catalogimg} alt={x.title} src={x['image-url']} />
-                    <Link to={`/products/${x._id}`}>{x.title}</Link>
-                </li>)}
-            </ul>
-            : <p>You have no products in your cart!</p>
-            }
-            <button onClick={clearCart}>Clear cart</button>
-        </div>
+            <div>
+                {Object.keys(cartItems).length > 0 ?
+                    <>
+                        <ul className={styles.cartul}>
+                            {Object.values(cartItems).map(x =>
+                                <li className={styles.cartli} key={x._id} >
+                                    <Link to={`/products/${x._id}`} ><img className={styles.cartimg} alt={x.title} src={x['image-url']} /></Link>
+                                    <Link className={`${styles.cartdetail} ${styles.carttitle}`} to={`/products/${x._id}`}>{x.title}</Link>
+                                    <p className={styles.cartdetail} >{x['product-price']}$</p>
+                                </li>)}
+                        </ul>
+                        <div className={styles.totalcontainer}>
+                            <p className={styles.cartdetail} >Total Price: 29.99$</p>
+                            <button className={styles.totalbutton}>Checkout</button>
+                            <button className={styles.totalbutton} onClick={clearCart}>Clear cart</button>
+                        </div>
+                    </>
+                    : <p>You have no products in your cart!</p>
+                }
+            </div>
         </>
     )
 }
