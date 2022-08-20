ee794fed130c8342459762c144abff4641d8e029
GIGABABAIT <zhhristov@abv.bg>
2022-08-20 20:11:16 +0300

    Login + Register styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,18 +4,20 @@ import { login } from "../services/authService"
 import { useNavigate } from "react-router-dom"
 import { AuthContext } from "../contexts/authContext"
 
+import styles from './LoginRegister.module.css'
+
 const Login = () => {
-    const {userLogin} = useContext(AuthContext)
+    const { userLogin } = useContext(AuthContext)
     const navigate = useNavigate()
 
     const onSubmit = (e) => {
         e.preventDefault()
-    
+
         const {
             email,
             password
         } = Object.fromEntries(new FormData(e.target))
-    
+
         login(email, password)
             .then(authData => {
                 if (!authData.code) {
@@ -26,29 +28,32 @@ const Login = () => {
                 }
             })
     }
-    
+
 
     return (
-        <div className="login-container">
-            <form id="login" onSubmit={onSubmit}>
-                <h1>Login</h1>
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="YourEmail@gmail.com"
-                />
-                <label htmlFor="login-pass">Password:</label>
-                <input type="password" id="login-password" name="password" />
-                <input type="submit" className="btn submit" value="Login" />
-                <p className="field">
-                    <span>
-                        If you do not have an account, click <Link to="/register">here</Link>.
-                    </span>
-                </p>
-            </form>
-        </div>
+        <>
+            <h1>Login</h1>
+
+            <div className={styles.container}>
+                <form className={styles.form} id="login" onSubmit={onSubmit}>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="YourEmail@gmail.com"
+                    />
+                    <label htmlFor="login-pass">Password:</label>
+                    <input type="password" id="login-password" name="password" />
+                    <input className={styles.button} type="submit" value="Login" />
+                    <p className="field">
+                        <span className={styles.haveaccount}>
+                            If you do not have an account, click <Link className={styles.redirect} to="/register">here.</Link>
+                        </span>
+                    </p>
+                </form>
+            </div>
+        </>
     )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom"
 import * as authService from '../services/authService'
 import { AuthContext } from "../contexts/authContext"
 
+import styles from './LoginRegister.module.css'
+
 const Register = () => {
 
     const { userLogin } = useContext(AuthContext)
@@ -12,48 +14,51 @@ const Register = () => {
 
     const onSubmit = (e) => {
         e.preventDefault()
-    
+
         const formData = new FormData(e.target)
-    
+
         const email = formData.get("email")
         const password = formData.get("password")
         const confirmPassword = formData.get("confirm-password")
-    
+
         if (password !== confirmPassword) {
             return
         }
-    
+
         authService.register(email, password)
             .then(authData => {
                 userLogin(authData)
                 navigate('/')
             })
     }
-    
+
 
     return (
-        <div className="register-container">
-            <form id="register" onSubmit={onSubmit}>
-                <h1>Register</h1>
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="YourEmail@gmail.com"
-                />
-                <label htmlFor="pass">Password:</label>
-                <input type="password" id="register-password" name="password" />
-                <label htmlFor="con-pass">Confirm Password:</label>
-                <input type="password" id="confirm-password" name="confirm-password" />
-                <input type="submit" className="btn submit" value="Register" />
-                <p className="field">
-                    <span>
-                        If you already have an account, click <Link to="/login">here</Link>.
-                    </span>
-                </p>
-            </form>
-        </div>
+        <>
+            <h1>Register</h1>
+
+            <div className={styles.container}>
+                <form className={styles.form} id="register" onSubmit={onSubmit}>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="YourEmail@gmail.com"
+                    />
+                    <label htmlFor="pass">Password:</label>
+                    <input type="password" id="register-password" name="password" />
+                    <label htmlFor="con-pass">Confirm Password:</label>
+                    <input type="password" id="confirm-password" name="confirm-password" />
+                    <input className={styles.button} type="submit" value="Register" />
+                    <p className="field">
+                        <span className={styles.haveaccount}>
+                            If you already have an account, click <Link className={styles.redirect} to="/login">here</Link>.
+                        </span>
+                    </p>
+                </form>
+            </div>
+        </>
     )
 }
 
